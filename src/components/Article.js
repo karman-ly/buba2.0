2007116da8d2b18b2aefe9e3a55b98fadf255f4b
Papsanly <papsanlysenko@gmail.com>
2022-09-23 20:59:46 +0300

    changed everything again

---
+++ src/components/Article.js
@@ -0,0 +1,42 @@
+import "./Article.css"
+
+export default function Article() {
+  return (
+      <article>
+
+        <h1>
+          Hello Sony
+        </h1>
+
+        <h1>
+          I Love You Sony And I Want Pines and Herb from you
+        </h1>
+
+        <h2>
+          Let's see
+        </h2>
+
+        <ul>
+          <li>I miss you</li>
+          <li>I need you</li>
+          <li>I *u** you</li>
+          <li>I love you</li>
+        </ul>
+
+        <p>
+          Сони буба не переживай скорьо через 2 недельки ми снова вмести и ми бу делать вмести всякие штучки, вмести
+          просипататси и гулять по класним местам и бу класи, так со убирай свою тревожньость и радуйся наступающим
+          праздникамь а их бу многя!
+        </p>
+
+        <h2>
+          Ухты ты датыкала до сюда
+        </h2>
+
+        <p className="small">
+          А уже всьо
+        </p>
+
+      </article>
+  )
+}
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import "./Button.css"
+
+export default function Button() {
+  return (
+      <div className="button-container">
+        <button>Press me (only for Sony)</button>
+      </div>
+  )
+}
