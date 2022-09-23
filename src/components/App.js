a30bf1625a62f8068787e83141315653326bcf8e
Papsanly <papsanlysenko@gmail.com>
2022-09-23 19:37:20 +0300

    changed everything

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Article from "./Article"
+import Button from "./Button"
+
+export default function App() {
+  return (
+      <div className="app">
+        <Article/>
+        <Button/>
+      </div>
+  );
+}
